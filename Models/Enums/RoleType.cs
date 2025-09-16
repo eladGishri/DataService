3d239270f4b2de5aee1b733d9ed75211a2c8c7f5
Elad Gishri <eladgishri@gmail.com>
2025-09-16 10:00:51 +0300

    Implement Model project. Containing entities, enums and exceptions

---
+++ Models/Enums/RoleType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataService.Models.Enums
+{
+    public enum RoleType
+    {
+        Admin, 
+        User
+    }
+}
---
+++ Models/Exceptions/UpdateOperationFailedException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Exceptions
+{
+    public class UpdateOperationFailedException : Exception
+    {
+        public UpdateOperationFailedException(string message) : base(message)
+        {
+
+        }
+    }
+}
