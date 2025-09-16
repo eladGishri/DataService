41cd0c823629999289980bb3f2c6346c0fc4dcec
Elad Gishri <eladgishri@gmail.com>
2025-09-16 10:05:39 +0300

    Implementation of ApplicationDbContext class for database storage provider

---
+++ Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using DataService.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace DataService.Infrastructure
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<DataEntity> DataEntities { get; set; } = null!;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<DataEntity>(entity =>
+            {
+                entity.Property(e => e.Value).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.CreatedAt).IsRequired();
+            });
+        }
+    }
+}
